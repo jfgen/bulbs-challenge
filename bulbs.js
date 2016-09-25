383eec9414863773b6c5233790c25ce827c001b1
Jorge Encarnação <jorge@dengun.com>
2016-09-25 13:56:18 +0100

    adding the code

---
+++ bulbs.js
@@ -0,0 +1,18 @@
+// This code is my attempt to answer the challenge at https://goo.gl/dShDe2
+
+var bulbsNum = 10; // Number of switches and bulbs
+var switches = [];
+
+// initializing the state of each switch
+for(i=0;i<10; i++) {
+	switches[i] = 0;
+	console.log(switches[i]);
+};
+
+//Getting the input 
+var input = "3 6
+0 4
+7 3
+9 9";
+ 
+console.log(input);
