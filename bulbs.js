a23d76306302ce0bd1d2ad29dfa3455714b376c8
Jorge Encarnação <jfgencarnacao@gmail.com>
2016-09-25 17:31:08 +0100

    almost there, need to figure out when the kid walks backwards

--- bulbs.js
+++ bulbs.js
@@ -4,15 +4,44 @@ var bulbsNum = 10; // Number of switches and bulbs
 var switches = [];
 
 // initializing the state of each switch
-for(i=0;i<10; i++) {
+for(i=0;i < bulbsNum; i++) {
 	switches[i] = 0;
-	console.log(switches[i]);
 };
 
-//Getting the input 
-var input = "3 6
-0 4
-7 3
-9 9";
- 
-console.log(input);
+//Getting the input
+var input =  [[3, 6],[0, 4], [7, 3], [9, 9]];
+
+inputConvert(input);
+
+function inputConvert(toggled) {
+	// Going through the input array and sending the intervals to the toggleSwitch function
+	for (var i = 0; i < toggled.length; i++) {
+		toggleSwitch(toggled[i][0], toggled[i][1]);
+	}
+};
+
+// Running through the switches again and comparing them with the input intervals, toggling them on or off depending on their initial state
+function toggleSwitch(firstToggle, lastToggle) {
+	for(i=0;i<bulbsNum; i++) {
+		if ( i >= firstToggle && i <= lastToggle) {
+			if(switches[i]===0) {
+				switches[i] = 1;
+			} else {
+				switches[i] = 0;
+			}
+		}
+	};
+};
+result();
+
+function result() {
+	var onSwitches = 0;
+
+	for(i=0;i < bulbsNum; i++) {
+		if (switches[i] !== 0) {
+				console.log(i);
+			onSwitches += 1;
+		};
+	};
+	console.log(onSwitches);
+};
